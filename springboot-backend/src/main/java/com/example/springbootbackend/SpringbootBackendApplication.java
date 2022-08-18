b8f9540b6813e57c068356d2dc143de1c6586420
Brandon Jones <brandonijones@outlook.com>
2022-08-18 18:37:57 -0400

    Move CorsConfigurationSource to SpringbootBackendApplication

--- springboot-backend/src/main/java/com/example/springbootbackend/SpringbootBackendApplication.java
+++ springboot-backend/src/main/java/com/example/springbootbackend/SpringbootBackendApplication.java
@@ -3,8 +3,13 @@ package com.example.springbootbackend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class SpringbootBackendApplication {
@@ -27,5 +32,18 @@ public class SpringbootBackendApplication {
 						.allowCredentials(false).maxAge(3600);
 			}
 		};
+	}
+
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "https://therecipebowl.netlify.app"));
+		configuration.setAllowedMethods(Arrays.asList("GET", "POST","PUT", "DELETE"));
+		configuration.setAllowedHeaders(Arrays.asList("Content-Type", "X-Requested-With", "accept", "Origin", "Access-Control-Request-Method",
+				"Access-Control-Request-Headers"));
+		configuration.setAllowCredentials(true);
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
 	}
 }
