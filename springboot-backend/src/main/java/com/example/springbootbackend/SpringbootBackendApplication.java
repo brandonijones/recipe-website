275adcfbfee29aad57d280e0cf89f039f239db9d
Brandon Jones <brandonijones@outlook.com>
2022-08-19 18:37:05 -0400

    fix: Attempt to resolve No web processes running
    Using this Stack Overflow answer
        - https://stackoverflow.com/questions/60085140/deploying-spring-boot-application-to-heroku-error-message-no-web-processes-ru
        - Updated Procfile
        - Added dependencies to pom.xml
        - Added method in application class

--- springboot-backend/src/main/java/com/example/springbootbackend/SpringbootBackendApplication.java
+++ springboot-backend/src/main/java/com/example/springbootbackend/SpringbootBackendApplication.java
@@ -3,11 +3,18 @@ package com.example.springbootbackend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
-public class SpringbootBackendApplication {
+public class SpringbootBackendApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootBackendApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringbootBackendApplication.class);
 	}
 }
