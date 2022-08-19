b334d5cbeb4c3e41efb6739d1803d57944375190
Brandon Jones <brandonijones@outlook.com>
2022-08-19 17:32:13 -0400

    style: Remove unused code in file

--- springboot-backend/src/main/java/com/example/springbootbackend/SpringbootBackendApplication.java
+++ springboot-backend/src/main/java/com/example/springbootbackend/SpringbootBackendApplication.java
@@ -3,9 +3,6 @@ package com.example.springbootbackend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class SpringbootBackendApplication {
@@ -13,17 +10,4 @@ public class SpringbootBackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootBackendApplication.class, args);
 	}
-
-//	@Bean
-//	public WebMvcConfigurer corsConfigurer() {
-//		return new WebMvcConfigurer() {
-//			@Override
-//			public void addCorsMappings(CorsRegistry registry) {
-//				registry.addMapping("/**")
-//						.allowedOrigins("https://therecipebowl.netlify.app", "http://localhost:3000", "http://192.168.56.1:3000")
-//						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-//						.allowedHeaders("*");
-//			}
-//		};
-//	}
 }
