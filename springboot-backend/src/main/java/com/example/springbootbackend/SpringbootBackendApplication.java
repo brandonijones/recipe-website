09a3a3934266c7374f102e7b3efed0ff3ad1a7d1
Brandon Jones <brandonijones@outlook.com>
2022-08-19 18:53:51 -0400

    Revert "fix: Attempt to resolve No web processes running"
    This reverts commit 275adcfbfee29aad57d280e0cf89f039f239db9d.

--- springboot-backend/src/main/java/com/example/springbootbackend/SpringbootBackendApplication.java
+++ springboot-backend/src/main/java/com/example/springbootbackend/SpringbootBackendApplication.java
@@ -3,18 +3,11 @@ package com.example.springbootbackend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
-public class SpringbootBackendApplication extends SpringBootServletInitializer {
+public class SpringbootBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootBackendApplication.class, args);
-	}
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(SpringbootBackendApplication.class);
 	}
 }
